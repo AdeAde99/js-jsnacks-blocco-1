97f3c7e54dcc0af75246245e31a65c8f53f9af4f
AdeAde99 <adelintanasa.d@gmail.com>
2022-02-28 14:56:11 +0100

    snack 1.1

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*Snack 1.1*/
+/*
+const num1 = parseInt(prompt('Numero1:'));
+const num2 = parseInt(prompt('Numero2:'));
+
+if (num1 > num2) {
+    alert(num1);
+} else if (num2 > num1) {
+    alert(num2);
+} else {
+    alert('sono uguali');
+}
+*/
+
+/*Snack 1.2*/
+
+const word1 = prompt('word1');
+const word2 = prompt('word2');
+const output = 
+
+/*
+let arrNum = [];
+
+for (i = 0; i < 10; i++) {
+
+}
+*/
